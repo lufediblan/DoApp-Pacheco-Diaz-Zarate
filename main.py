7f886bcd9ecc1493a4b0caea459fee152c01014b
Luis Diaz <lufediblan@gmail.com>
2025-08-08 07:40:00 -0500

    Inicializar proyecto ToDoApp

---
+++ main.py
@@ -0,0 +1,8 @@
+from task_model import TaskModel
+
+def main():
+    task_model = TaskModel("Estudiar para el examen")
+    print(f"Tarea: {task.get_task_name()}")
+
+if __name__ == "__main__":
+    main()
